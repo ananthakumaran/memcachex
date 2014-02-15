22f6c78e2e2e0aa729b390f120318b5a71dbb471
Anantha Kumaran <ananthakumaran@gmail.com>
2014-02-15 13:28:12 +0530

    add some test

--- test/memcache_test.exs
+++ test/memcache_test.exs
@@ -1,7 +1,13 @@
 defmodule MemcacheTest do
   use ExUnit.Case
+  alias Memcache.Connection
 
-  test "the truth" do
-    assert(true)
+  test "commands" do
+    { :ok, pid } = Connection.start_link([ hostname: "localhost" ])
+    cases = [{:get, ["unknown"], "Not found"}]
+
+    Enum.each(cases, fn ({ command, args, response }) ->
+      assert(Connection.execute(pid, command, args) == { :ok, response })
+    end)
   end
 end
