3bc642ae524f1526ecd527328617cffe48cf621c
Jovany Leandro G.C <bit4bit@riseup.net>
2024-04-28 15:12:21 +0300

    refactor: add concept Transport

--- lib/memcache/utils.ex
+++ lib/memcache/utils.ex
@@ -11,7 +11,7 @@ defmodule Memcache.Utils do
 
   def format_error(reason) do
     case :inet.format_error(reason) do
-      'unknown POSIX error' -> inspect(reason)
+      ~c"unknown POSIX error" -> inspect(reason)
       message -> List.to_string(message)
     end
   end
